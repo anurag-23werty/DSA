aafc816a6cd77ba14960949c7fed21a5a55e8274
anurag-23werty <anurag.omm11@gmail.com>
2026-01-25 23:55:47 +0530

    Time: 21 ms (60.33%) | Memory: 17.4 MB (54.17%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -25,11 +25,11 @@ public:
                     }
                     else if(sum<target){
                         k++;
-                        while(k<l && nums[k]==nums[k-1]) k++;
+                        // while(k<l && nums[k]==nums[k-1]) k++;
                     }
                     else{
                         l--;
-                        while(k<l && nums[l]==nums[l+1]) l--;
+                        // while(k<l && nums[l]==nums[l+1]) l--;
                     }
 
                 }
