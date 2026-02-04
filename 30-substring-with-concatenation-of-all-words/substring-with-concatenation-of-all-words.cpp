3783c85aba0d2b716d72194c8633ee146c8e5f9d
anurag-23werty <anurag.omm11@gmail.com>
2026-02-04 19:25:46 +0530

    Time: 28 ms (30.39%) | Memory: 23.8 MB (82.09%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int n=s.size();
+        vector<int>result;
+        int numwords=words.size();
+        if(n==0 || numwords==0) return result;
+        int wordlen=words[0].size();
+        int totalLen=numwords*wordlen;
+        if(n<totalLen) return result;
+        unordered_map<string,int>required;
+        for(auto &w:words){
+            required[w]++;
+        }
+        for(int i=0;i<wordlen;i++){
+            int left=i;
+            int matched=0;
+            unordered_map<string,int>seen;
+            for(int j=i;j+wordlen<=n;j+=wordlen){
+                string word=s.substr(j,wordlen);
+                if(required.count(word)){
+                    seen[word]++;
+                    if(seen[word]<=required[word]){
+                        matched++;
+                    }
+                    while(seen[word]>required[word]){
+                        string str=s.substr(left,wordlen);
+                        seen[str]--;
+                        if(seen[str]<required[str]) matched--;
+                        left+=wordlen;
+                    }
+                    if(matched==numwords) result.push_back(left);
+                }
+                else{
+                    seen.clear();
+                    matched=0;
+                    left=j+wordlen;
+                }
+
+            }
+        }
+        return result;
+
+        
+    }
+};
