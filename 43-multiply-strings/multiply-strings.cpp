fbcdcdcddb47f491f6e6ed285c73e0b77af279b4
anurag-23werty <anurag.omm11@gmail.com>
2026-03-04 22:35:59 +0530

    Time: 3 ms (58.71%) | Memory: 10.7 MB (27.71%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1=="0" || num2=="0") return "0";
+        vector<int>res(num1.length()+num2.length(),0);
+        
+        for(int i=num1.length()-1;i>=0;i--){
+            for(int j=num2.length()-1;j>=0;j--){
+                res[i+j+1]+=(num1[i]-'0')*(num2[j]-'0');
+                res[i+j]+=res[i+j+1]/10;
+                res[i+j+1]%=10;
+
+            }
+        }
+        string str="";
+        for(int i=0;i<res.size();i++){
+            if(!(str.empty() && res[i]==0)){
+            str+=to_string(res[i]);
+            }
+        }
+        return str;
+        
+    }
+};
