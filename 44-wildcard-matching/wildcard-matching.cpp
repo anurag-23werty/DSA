7d3c6d52fc30314cbdc9128411bebf6fce169073
anurag-23werty <anurag.omm11@gmail.com>
2026-03-07 12:45:15 +0530

    Time: 35 ms (78.06%) | Memory: 10.4 MB (79.43%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -19,10 +19,10 @@ public:
     bool isMatch(string s, string p) {
         int m=s.length();
         int n=p.length();
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
-        dp[0][0]=true;
+        vector<bool>prev(m+1,false),curr(m+1,false);
+        prev[0]=true;
         for(int j=1;j<=m;j++){
-            dp[0][j]=false;
+            prev[j]=false;
         }
         for(int i=1;i<=n;i++){
             int flag=true;
@@ -32,16 +32,19 @@ public:
                     break;
                 }
             }
-            dp[i][0]=flag;
-        }
-        for(int i=1;i<=n;i++){
+            curr[0]=flag;
+        
+        
             for(int j=1;j<=m;j++){
-                if(p[i-1]==s[j-1]||p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
-                else if(p[i-1]=='*') dp[i][j]=dp[i-1][j]|dp[i][j-1];
-                else dp[i][j]=false;
+                if(p[i-1]==s[j-1]||p[i-1]=='?') curr[j]=prev[j-1];
+                else if(p[i-1]=='*') curr[j]=prev[j]|curr[j-1];
+                else curr[j]=false;
             }
+        
+        prev=curr;
         }
-        return dp[n][m];
+
+        return prev[m];
         
     }
 };
