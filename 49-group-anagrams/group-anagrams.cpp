7d78d46aa82457e590de80af0e39d9f2f63bbe83
anurag-23werty <anurag.omm11@gmail.com>
2026-03-13 00:45:46 +0530

    Time: 20 ms (38.22%) | Memory: 24.9 MB (86.83%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<map<char,int>,vector<string>>Hashmap;
+        // map<map<char,int>,vector<string>>Hashmap;
+        // for(string str:strs){
+        //     map<char,int>mapp;
+        //     for(int i=0;i<str.length();i++){
+        //         mapp[str[i]]++;
+        //     }
+        //     Hashmap[mapp].push_back(str);
+
+
+        // }
+        // vector<vector<string>>ans;
+        // for(auto &j:Hashmap){
+        //     ans.push_back(j.second);
+        // }
+        // return ans;
+        unordered_map<string,vector<string>>Hash;
         for(string str:strs){
-            map<char,int>mapp;
-            for(int i=0;i<str.length();i++){
-                mapp[str[i]]++;
-            }
-            Hashmap[mapp].push_back(str);
-
-
+            string key=str;
+            sort(key.begin(),key.end());
+            Hash[key].push_back(str);
         }
-        vector<vector<string>>ans;
-        for(auto &j:Hashmap){
-            ans.push_back(j.second);
+        vector<vector<string>>result;
+        for(auto &j:Hash){
+            result.push_back(j.second);
         }
-        return ans;
+        return result;
         
     }
 };
