51394873508f62454c1dc25e9e027430af57b114
anurag-23werty <anurag.omm11@gmail.com>
2026-03-15 12:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (52.16%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,20 +1,4 @@
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         int maci=INT_MIN;
-//         int sum=0;
-//         for(int i=0;i<nums.size();i++){
-            
-//             sum+=nums[i];
-//             if(sum>maci) maci=sum;
-//             if(sum<0) sum=0;
-//         }
-//         if(maci<0) return -1;
-//         return maci;
 
-        
-//     }
-// };
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
