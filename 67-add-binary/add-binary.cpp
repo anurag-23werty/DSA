c9611db5e700c7f6102d058e67976a544c96b949
Anurag Choudhury <anurag.omm11@gmail.com>
2026-04-24 17:02:33 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (60.64%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int m=a.size()-1;
+        int n=b.size()-1;
+        string result="";
+        int carry=0;
+        int sum=0;
+        while(m>=0 || n>=0){
+            sum=carry;
+            if(m>=0){
+                int ans=a[m--]-'0';
+                sum+=ans;
+            }
+            if(n>=0){
+                int ans2=b[n--]-'0';
+                sum+=ans2;
+            }
+            result+=((sum%2==0)?'0':'1');
+            carry=((sum>1)?1 : 0);
+
+        } 
+        if(carry) result+='1';
+        reverse(begin(result),end(result));
+        return result;
+        
+    }
+};
