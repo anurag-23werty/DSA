b1c2c164592f3c959ea3d29e37ce51bb7f527f63
Anurag Choudhury <anurag.omm11@gmail.com>
2026-07-16 19:43:00 +0530

    Time: 116 ms (21.93%) | Memory: 99.8 MB (15.05%) - LeetSync

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        unordered_set<int>st;
+        int i = 0;
+        long long curSum=0;
+        long long maxi=0;
+        
+        for(int j=0;j<nums.size();j++){
+            while(st.count(nums[j])==1 || st.size()==k){
+                st.erase(nums[i]);
+                curSum-=nums[i];
+                i++;
+            }
+            st.insert(nums[j]);
+            curSum+=nums[j];
+            if(st.size()==k){
+                maxi = max(maxi,curSum);
+            }
+
+        }
+        return maxi;
+
+    }
+};
