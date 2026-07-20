6bd8fbaf7b2622c27ceddb4d643e7238e99c8098
Anurag Choudhury <anurag.omm11@gmail.com>
2026-07-21 02:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (87.24%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.cpp
+++ 457-circular-array-loop/circular-array-loop.cpp
@@ -17,38 +17,63 @@ public:
     }
     bool circularArrayLoop(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
-            unordered_set<int>st;
-            st.insert(i);
+            // unordered_set<int>st;
+            // st.insert(i);
+             if(nums[i]==0) continue;
             int pos = nums[i]>0;
             int curr=i;
-            if(nums[curr]==0) continue;
-
-            while(true){
-                int next = nextIndex(curr,nums);
+            int slow=i;
+            int fast =i;
+            do{
+                slow = nextIndex(slow,nums);
+                fast = nextIndex(fast,nums);
                 if(pos){
-                    if(nums[next]<0) break;
-                    else{
-                        if(st.find(next)!=st.end()){
-                            if(curr!=next) return true;
-                            else break;
-                        }
-                        
-                        st.insert(next);
-                    }
+                    if(nums[fast]<0) break;
+
                 }
                 else{
-                    if(nums[next]>0) break;
-                    else{
-                        if(st.find(next)!=st.end()){
-                            if(curr!=next) return true;
-                            else break;
-                        }
-                        st.insert(next);
-                    }
+                    if(nums[fast]>0) break;
                 }
-                curr=next;
+                fast = nextIndex(fast,nums);
+                if(pos){
+                    if(nums[fast]<0) break;
 
-            }
+                }
+                else{
+                    if(nums[fast]>0) break;
+                }
+                if(slow==fast){
+                    if(slow == nextIndex(slow,nums)) break;
+                    else return true;
+                }
+            }while(slow!=fast);
+
+            // while(true){
+            //     int next = nextIndex(curr,nums);
+            //     if(pos){
+            //         if(nums[next]<0) break;
+            //         else{
+            //             if(st.find(next)!=st.end()){
+            //                 if(curr!=next) return true;
+            //                 else break;
+            //             }
+                        
+            //             st.insert(next);
+            //         }
+            //     }
+            //     else{
+            //         if(nums[next]>0) break;
+            //         else{
+            //             if(st.find(next)!=st.end()){
+            //                 if(curr!=next) return true;
+            //                 else break;
+            //             }
+            //             st.insert(next);
+            //         }
+            //     }
+            //     curr=next;
+
+            // }
             curr = i;
             if(pos){
                 while(nums[curr]>0){
