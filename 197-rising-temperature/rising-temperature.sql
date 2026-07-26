fa639925a378164b0814aa89fab964148a0ad1bc
Anurag Choudhury <anurag.omm11@gmail.com>
2026-07-26 20:05:46 +0530

    Time: 263 ms (38.49%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select today.id from Weather as today
+where exists (select 1 from Weather as yesterday
+        where yesterday.temperature<today.temperature
+        and 
+        today.recordDate - yesterday.recordDate=1)
