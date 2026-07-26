67f3111b148bb307f711b46aef5b0be2c66f6ca3
Anurag Choudhury <anurag.omm11@gmail.com>
2026-07-27 01:52:27 +0530

    Time: 195 ms (94.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+select
+p.product_id,
+(case 
+when sum(u.units) is null 
+then 0
+else round(sum(u.units*p.price)/sum(u.units)::numeric,2)
+end
+) as average_price
+from
+Prices p
+left join UnitsSold u
+on u.product_id = p.product_id
+and u.purchase_date  between p.start_date and p.end_date
+group by
+p.product_id;
