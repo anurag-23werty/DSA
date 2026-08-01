d2af09c71dca5386ebbd67cdadff327246a6d9ab
Anurag Choudhury <anurag.omm11@gmail.com>
2026-08-02 01:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (36.80%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -17,12 +17,12 @@ public:
                 }
             }
             else{
-                if(target<=nums[end] && target>=nums[mid]){
-                    start=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
+                if(target > nums[mid] && target <= nums[end]){
+    start = mid + 1;
+}
+else{
+    end = mid - 1;
+}
 
             }
         }
