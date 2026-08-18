a8e0ab8d5bf0b9d254bc3679eceb06bf4e187e74
Anurag Choudhury <anurag.omm11@gmail.com>
2026-08-18 17:03:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (39.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,11 +4,11 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         int cntFresh =0 ;
-        queue<pair<pair<int,int>,int>>q;
+        queue<pair<int,int>>q;
         vector<vector<int>>visited(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2) {q.push({{i,j},0});
+                if(grid[i][j]==2) {q.push({i,j});
                 visited[i][j]=2;
                 }
                 
@@ -18,26 +18,27 @@ public:
 
         int drow[] = {-1,0,1,0};
         int urow[] ={0,-1,0,1};
-        int cnt = 0;
+        
         int time = 0;
-        while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow = r+drow[i];
-                int ncol = c+urow[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol]==1){
-                    visited[nrow][ncol] = 1;
-                    time = t+1;
-                    q.push({{nrow,ncol},t+1});
-                    cnt++;
+        while(!q.empty() && cntFresh>0){
+            int size = q.size();
+            while(size--){
+                auto [r,c]=q.front();
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int newR = r+drow[i];
+                    int newC = c+urow[i];
+                    if(newR>=0 && newR<n && newC>=0 && newC<m && !visited[newR][newC] && grid[newR][newC]==1){
+                        q.push({newR,newC});
+                        visited[newR][newC] = 2;
+                        cntFresh--;
+
+                    }
                 }
             }
+            time++;
         }
-        if(cnt!=cntFresh) return -1;
-        return time;
+        return cntFresh==0?time :-1;
 
 
 
